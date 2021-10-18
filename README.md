e93e817daddbef8c97ee8184f9b5317beddde76e
MostsfaMGomaa <gomaamostafa26@gmail.com>
2021-10-18 08:49:07 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# TODO
+Simple and temporarily todo list
+
+## How list workrs
+- Add  your tasks by press Add button or hit enter key
+- Clear your list by press clear button
---
+++ script.js
@@ -0,0 +1,56 @@
+'use strict'
+const heaing = document.querySelector('.heading');
+const txt = document.querySelector('.txt');
+const addTask = document.querySelector('.add');
+const welcomWord = document.querySelector('.welcom');
+const invaildMessage = document.querySelector('.invaild');
+const newTask = document.querySelector('.newTask');
+const clearTasks = document.querySelector('.clear');
+if (!localStorage.getItem('tasks')) {
+  localStorage.setItem('tasks', JSON.stringify('[]'));
+}
+let tasks = localStorage.getItem("tasks");
+function add(txt) {
+  if (txt.value.length > 1) {
+    console.log(tasks);
+    tasks += ' ' + txt.value
+    console.log(tasks);
+    // hide error message, welocm word
+    invaildMessage.classList.add('hidden');
+    welcomWord.classList.add('hidden');
+    // add new p
+    const para = document.createElement("p");
+    const node = document.createTextNode(txt.value);
+    para.appendChild(node);
+    // area in which paragraph appers
+    newTask.appendChild(para);
+    //clear txt area after add task
+    txt.value = '';
+    // apper clear button
+    clearTasks.classList.remove('hidden');
+  }
+  else {
+    // show error message.
+    invaildMessage.classList.remove('hidden');
+  }
+}
+
+// Add task by button
+addTask.addEventListener('click', function () {
+  add(txt);
+})
+
+// Add task by pressing enter
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Enter') add(txt);
+})
+
+clearTasks.addEventListener('click', function () {
+  // clear the div
+  newTask.innerHTML = '';
+  // Add welcom word, hide clear button and hide error meassage
+  welcomWord.classList.remove('hidden');
+  clearTasks.classList.add('hidden');
+  invaildMessage.classList.add('hidden');
+
+})
